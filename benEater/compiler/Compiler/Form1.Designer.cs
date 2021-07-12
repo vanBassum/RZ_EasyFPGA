1b2f947718c62124ab8df310aa668369614197db
Bas Visscher <bas.visscher24@gmail.com>
2021-07-12 16:53:37 +0200

    Supplied the compiler with an example

--- benEater/compiler/Compiler/Form1.Designer.cs
+++ benEater/compiler/Compiler/Form1.Designer.cs
@@ -33,21 +33,29 @@ namespace Compiler
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(282, 387);
+            this.richTextBox1.Size = new System.Drawing.Size(282, 381);
             this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.richTextBox1.Text = "START:\nLDI 5\nOUT\n\nSUBSTRACT:\nSUI 1\nOUT\nJNZ SUBSTRACT\n\nDONE:\nLDI 8\nOUT\nHLT";
             // 
             // richTextBox2
             // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox2.Location = new System.Drawing.Point(381, 12);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(282, 387);
+            this.richTextBox2.Size = new System.Drawing.Size(520, 381);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
@@ -57,29 +65,43 @@ namespace Compiler
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Compile";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(12, 405);
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox3.Location = new System.Drawing.Point(12, 399);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(659, 165);
+            this.richTextBox3.Size = new System.Drawing.Size(889, 165);
             this.richTextBox3.TabIndex = 3;
             this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Help";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 582);
+            this.ClientSize = new System.Drawing.Size(913, 576);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ASM compiler";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -90,6 +112,7 @@ namespace Compiler
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ benEater/compiler/Compiler/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<Instruction> instructions = new List<Instruction>()
+        {
+            new Instruction("LDI", 0x01, 1, "Writes value to REGA"),
+            new Instruction("LDA", 0x02, 1, "Reads RAM to REGA"),
+            new Instruction("STA", 0x03, 1, "Writes REGA to RAM"),
+            new Instruction("OUT", 0x04, 0, "Writes REGA to Output"),
+            new Instruction("ADI", 0x05, 1, "Increases REGA by value"),
+            new Instruction("ADD", 0x06, 1, "Increases REGA by RAM"),
+            new Instruction("JP",  0x07, 1, "Jumps to address or label"),
+            new Instruction("JNZ", 0x08, 1, "Jumps to address or label if REGA is not zero"),
+            new Instruction("SUI", 0x09, 1, "Decreases REGA by value"),
+            new Instruction("HLT", 0xFF, 1, "Stops code execution"),
+        };
+
+
+
+
+        void ASMERROR(int lineNo, string msg)
+        {
+            richTextBox3.AppendText($"Line {lineNo}: {msg}\r\n");
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rd = 0;
+            byte wr = 0;
+            richTextBox2.Text = "";
+            Dictionary<string, byte> labels = new Dictionary<string, byte>();
+
+            for (rd = 0; rd < richTextBox1.Lines.Length; rd++)
+            {
+                string line = richTextBox1.Lines[rd];
+                string[] split = line.Split(' ');
+
+                if (split.Length > 0)
+                {
+                    if (split[0].EndsWith(":"))
+                    {
+                        labels.Add(split[0].TrimEnd(':'), wr);
+                    }
+                    else
+                    {
+                        string instr = split[0].Trim('\t', ' ');
+
+                        if(instr != "") //Ignore white spaces
+                        {
+                            Instruction ist = instructions.FirstOrDefault(a => a.ASM == split[0]);
+
+                            if (ist == null)
+                            {
+                                ASMERROR(rd, $"Instruction '{split[0]}' not found.");
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    string s;
+                                    ist.ToString(ref wr, out s, split, labels);
+                                    richTextBox2.AppendText(s);
+                                }
+                                catch (Exception ex)
+                                {
+                                    ASMERROR(rd, ex.Message);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "Available commands:\r\n";
+
+            foreach(Instruction ist in instructions)
+            {
+                richTextBox2.AppendText($" - {ist.ASM.PadRight(4)} takes {ist.Parameters} parameters. {ist.Description}\r\n");
+            }
+
+        }
+    }
+
+    class Instruction
+    {
+        public string ASM { get; set; } = "HLT";
+        public byte HEX { get; set; } = 0xFF;
+        public string Description { get; set; } = "Stops exectution";
+        public int Parameters { get; set; } = 0;
+
+        public Instruction()
+        {
+
+        }
+
+        public Instruction(string aSM, byte hEX, int parameters, string description)
+        {
+            ASM = aSM;
+            HEX = hEX;
+            Description = description;
+            Parameters = parameters;
+        }
+
+        public void ToString(ref byte wr, out string s, string[] parameters, Dictionary<string, byte> labels)
+        {
+            s = $"{wr++} => x\"{HEX.ToString("X2")}\", \t--{Description}\r\n";
+
+            for (int i = 1; i < parameters.Length; i++)
+            {
+                byte p;
+                if (byte.TryParse(parameters[i], out p))
+                    s += $"{wr++} => x\"{p.ToString("X2")}\",\r\n";
+                else if(labels.TryGetValue(parameters[i], out p))
+                    s += $"{wr++} => x\"{p.ToString("X2")}\",\r\n";
+                else
+                    throw new Exception($"Parameter {i} '{parameters[i]}' is not a valid label nor convertable to a byte!");
+            }
+        }
+    }
+}
